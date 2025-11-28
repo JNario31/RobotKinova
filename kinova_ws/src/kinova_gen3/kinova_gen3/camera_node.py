40e73c24afb13accc90e66628f089743278c8b1a
John Nario <johnnario@Johns-MacBook-Air.local>
2025-11-27 19:48:50 -0500

    Added 3 calibration squares

--- kinova_ws/src/kinova_gen3/kinova_gen3/camera_node.py
+++ kinova_ws/src/kinova_gen3/kinova_gen3/camera_node.py
@@ -78,13 +78,13 @@ class CameraNode(Node):
         # Compute 2D affine transform
         matrix = self.compute_affine_from_three_points(blue_square_pixels, BLUE_SQUARE_WORLD)
         self.get_logger().info(f"Affine transformation matrix (2x3):\n{matrix}")
-        
+
         # Get the four corners of the blue square bounding box
-        for blues in blue_squares:
-            x_center = blue_square_pred['x']
-            y_center = blue_square_pred['y']
-            width = blue_square_pred['width']
-            height = blue_square_pred['height']
+        # for blues in blue_squares:
+        #     x_center = blue_square_pred['x']
+        #     y_center = blue_square_pred['y']
+        #     width = blue_square_pred['width']
+        #     height = blue_square_pred['height']
             
         
         # Measure your actual blue square and put the dimensions here!
