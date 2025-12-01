c79801b82b46b6533d0b1a85667d5054f77edb1b
John Nario <johnnario@Johns-MacBook-Air.local>
2025-12-01 18:42:56 -0500

    camera 4

--- kinova_ws/src/kinova_gen3/kinova_gen3/kinova_gen3_tester.py
+++ kinova_ws/src/kinova_gen3/kinova_gen3/kinova_gen3_tester.py
@@ -117,7 +117,7 @@ def take_picture():
     camera_matrix = calib['camera_matrix']
     dist_coeffs = calib['dist_coeffs']
 
-    cap = cv2.VideoCapture(5)
+    cap = cv2.VideoCapture(4)
 
     ret, frame = cap.read()
         
